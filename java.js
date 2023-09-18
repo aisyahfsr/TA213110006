3ba5bb49695f5263a3da0c89a7cdcdbcdd68cb10
Sisi Auliani <singkongsisi@gmail.com>
2023-09-18 10:21:11 -0700

    add code

---
+++ java.js
@@ -0,0 +1,34 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky", this.window.scrollY >0);
+})
+
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+}
+
+// Select all heart icons
+const heartIcons = document.querySelectorAll('.heart-icon i');
+
+// Add a click event listener to each heart icon
+heartIcons.forEach(heartIcon => {
+    heartIcon.addEventListener('click', function() {
+        const isFavorite = this.getAttribute('data-favorite') === 'true';
+
+        // Toggle the favorite state
+        if (isFavorite) {
+            this.setAttribute('data-favorite', 'false');
+            this.classList.remove('fas'); // Remove filled heart
+            this.classList.add('far');    // Add empty heart
+        } else {
+            this.setAttribute('data-favorite', 'true');
+            this.classList.remove('far');    // Remove empty heart
+            this.classList.add('fas');       // Add filled heart
+        }
+    });
+});
